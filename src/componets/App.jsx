ac8bf370c7cc65941a3f56a56e3d1e283165137d
FadiAnd1610 <fadi9844@gmail.com>
2024-01-18 00:08:43 +0200

    this is working trust me

--- src/componets/App.jsx
+++ src/componets/App.jsx
@@ -20,36 +20,69 @@ import Violnce from "./Big-Subjects/violnce";
 import Partners from "./Big-Subjects/purtners";
 import Gun from "./Big-Subjects/gun";
 function App() {
-  let Componet;
+  let Component;
+
   switch (window.location.pathname) {
-    case "/":
-      Componet = <App />;
-      break;
     case "/contant":
-      Componet = <Contant />;
+      Component = <Contant />;
       break;
     case "/contact":
-      Componet = <Contact />;
+      Component = <Contact />;
       break;
     case "/home":
-      Componet = <Home />;
+      Component = <Home />;
       break;
     case "/about":
-      Componet = <About />;
+      Component = <About />;
       break;
     case "/adjust":
-      Componet = <Card />;
+      Component = <Card />;
       break;
-
+    case "/Team":
+      Component = <Tax />;
+      break;
+    case "/now":
+      Component = <CriminalRepresentation />;
+      break;
+    case "/child":
+      Component = <Minors />;
+      break;
+    case "/robery":
+      Component = <Robery />;
+      break;
+    case "/pardons":
+      Component = <Forgive />;
+      break;
+    case "/murder":
+      Component = <Murder />;
+      break;
+    case "/dead":
+      Component = <Dead />;
+      break;
+    case "/car":
+      Component = <Car />;
+      break;
+    case "/sex":
+      Component = <Sex />;
+      break;
+    case "/violnce":
+      Component = <Violnce />;
+      break;
+    case "/partners":
+      Component = <Partners />;
+      break;
+    case "/gun":
+      Component = <Gun />;
+      break;
     default:
-      Componet = <Home />;
+      Component = <Home />;
       break;
   }
 
   return (
     <div className="App">
       <NavScrollExample />
-      {Componet}
+      {Component}
 
       <Router>
         <Switch>
