6c69d88230e75352f876a3e538ff9ce770923288
FadiAnd1610 <fadi9844@gmail.com>
2024-01-20 11:27:54 +0200

    newfix

--- src/componets/contant.jsx
+++ src/componets/contant.jsx
@@ -21,7 +21,8 @@ function Contant() {
                 href="https://www.ynet.co.il/articles/0,7340,L-4341057,00.html"
                   target="_blank"
                   rel="noreferrer"
-                className="press"
+                  className="press"
+                  style={{textAlign:'center'}}
               >
                  זוג עורכי הדין,החשד לגניבה והרב הבכיר 
               </a>
@@ -42,7 +43,8 @@ function Contant() {
                 href="https://www.ynet.co.il/articles/0,7340,L-4053728,00.html"
                   target="_blank"
                   rel="noreferrer"
-                className="press"
+                  className="press"
+                  style={{textAlign:'center'}}
               >
                   עסק משפחתי: אחים נתפסו 'על חם' פורצים לבית 
               </a>
@@ -51,6 +53,7 @@ function Contant() {
               href="https://www.ynet.co.il/articles/0,7340,L-4053728,00.html"
                 target="_blank"
                 rel="noreferrer"
+                style={{textAlign:'center'}}
             >
               <img src={art2} alt="article number 1" />
             </a>
@@ -63,7 +66,8 @@ function Contant() {
                 href="https://13tv.co.il/item/news/domestic/crime-and-justice/ramat-hasharon-903660219/"
                   target="_blank"
                   rel="noreferrer"
-                className="press"
+                  className="press"
+                  style={{textAlign:'center'}}
               >
                   חשד: עובדי עירייה הקימו אתר פסולת פיראטית - וגרפו הון לכיסם 
               </a>
@@ -88,7 +92,8 @@ function Contant() {
                 href="https://www.ynet.co.il/news/article/B1gprmNWw"
                   target="_blank"
                   rel="noreferrer"
-                className="press"
+                  className="press"
+                  style={{textAlign:'center'}}
               >
                   איש העסקים מת"א שחשוד בהזנחת התינוק: "הוא החיים שלי. לא יודע איך הגיעו אליו סמים" 
               </a>
@@ -97,6 +102,7 @@ function Contant() {
               href="https://www.ynet.co.il/news/article/B1gprmNWw"
                 target="_blank"
                 rel="noreferrer"
+                style={{textAlign:'center'}}
             >
               <img src={art4} alt="article number 1" />
             </a>
@@ -109,7 +115,8 @@ function Contant() {
                 href="https://www.bhol.co.il/news/745065"
                   target="_blank"
                   rel="noreferrer"
-                className="press"
+                  className="press"
+                  style={{textAlign:'center'}}
               >
                   מעצר עורכי הדין החרדים הוארך: "גניבה בהיקף גדול" 
               </a>
@@ -126,7 +133,8 @@ function Contant() {
                 href="https://www.ynet.co.il/news/article/bjymnwok5"
                   target="_blank"
                   rel="noreferrer"
-                className="press"
+                  className="press"
+                  style={{textAlign:'center'}}
               >
                   החשוד המרכזי בדריסת המתנדב יישאר במעצר, היתר שוחררו: "צעקנו שיבלום"
               </a>
@@ -135,6 +143,7 @@ function Contant() {
               href="https://www.ynet.co.il/news/article/bjymnwok5"
                 target="_blank"
                 rel="noreferrer"
+                style={{textAlign:'center'}}
             >
               <img src={art6} alt="article number 1" />
             </a>
